96e7f6be03a5e74e7f4c0377df2267b866ecff6d
Pasindu Perera <pasindubawantha@gmail.com>
2018-12-11 14:16:21 +0530

    Changed AR Model training method

--- nab/detectors/TSA_AR/AR.py
+++ nab/detectors/TSA_AR/AR.py
@@ -15,15 +15,6 @@ class AR:
         self.p=p 
         return 0
     
-    def set_training_data_time(self, time):
-        self.training_data_time = time
-    
-    def set_validation_data_time(self, time):
-        self.validation_data_time = time
-        
-    def set_testing_data_time(self, time):
-        self.testing_data_time = time
-    
     def set_validation_data_set(self,data):
         self.validation_data_set = data
         
@@ -77,153 +68,21 @@ class AR:
         self.prediction = self.prediction.transpose()[0] + np.mean(data_set)
         return self.prediction
     
+    def get_single_prediction(self,data_set,time):
+        mean = np.mean(data_set)
+        Z = data_set - mean
+        prediction = np.dot(self.calculate_normal_matrix_x_row(Z, time), self.weights)
+        prediction = prediction[0][0]
+        prediction = prediction + mean
+        return prediction
+
     # Diagnostics and identification messures
-    def mse(self,values,pridicted):
+    def get_mse(self, values, prediction):
         error = 0.0
         for i in range(0,len(values)):
-            error += (values[i] - pridicted[i])**2
+            error += (values[i] - prediction[i])**2
         return error/len(values)
-    
-    def get_mse(self, data, prediction):
-        return self.mse(data,prediction)
-    
-    def plot_autocorrelation(self, data_set, lag):
-        autocorrelations = np.zeros(lag)
-        autocorrelations_x = np.arange(lag)
-        autocorrelations[0] = 1.0
-        for i in range(1,lag):
-            autocorrelations[i] = np.corrcoef(data_set[i:],data_set[:-i])[0,1]
-        
-        trace = {"x": autocorrelations_x,
-                 "y": autocorrelations,
-                 'type': 'bar',
-                 "name": 'Autocorrelation',         
-                }
-        
-        traces = [trace]
-        layout = dict(title = "Autocorrelation",
-                  xaxis = dict(title = 'Lag'),
-                  yaxis = dict(title = 'Autocorrelation')
-                 )
-        fig = dict(data=traces, layout=layout)
-        # iplot(fig)
-    
-    def plot_partial_autocorrelation(self, data_set, lag):
-        pac = np.zeros(lag)
-        pac_x = np.arange(lag)
-        
-        residualts = data_set
-        slope, intercept = np.polyfit(data_set,residualts,1)
-        estimate = intercept + slope*data_set
-        residualts = residualts - estimate
-        pac[0] = 1
-        for i in range(1,lag):
-            pac[i] = np.corrcoef(data_set[:-i],residualts[i:])[0,1]
-            
-            slope, intercept = np.polyfit(data_set[:-i],residualts[i:],1)
-            estimate = intercept + slope*data_set[:-i]
-            
-            residualts[i:] = residualts[i:] - estimate
-        
-        trace = {"x": pac_x,
-                 "y": pac,
-                 'type': 'bar',
-                 "name": 'Partial Autocorrelation',         
-                }
 
-        traces = [trace]
-        layout = dict(title = "Partial Autocorrelation",
-                  xaxis = dict(title = 'Lag'),
-                  yaxis = dict(title = 'Partial Autocorrelation')
-                 )
-        fig = dict(data=traces, layout=layout)
-        # iplot(fig)
-    
-    def plot_residuals(self, data_set, prediction):
-        x = np.arange(len(data_set))
-        residual = data_set - prediction
-        mean = np.ones(len(data_set))*np.mean(residual)
-        
-        trace = {"x": x,
-                 "y": residual,
-                 "mode": 'markers',
-                 "name": 'Residual'}
-
-        trace_mean = {"x": x,
-                     "y": mean,
-                     "mode": 'lines',
-                     "name": 'Mean'}
-        traces = [trace,trace_mean]
-        layout = dict(title = "Residual",
-                      xaxis = dict(title = 'X'),
-                      yaxis = dict(title = 'Residual')
-                     )
-        fig = dict(data=traces, layout=layout)
-        # iplot(fig)
-        print("Standard Deviation of Residuals : " + str(np.std(residual, ddof=1)))
-        print("Mean of Residuals : " + str(np.mean(residual)))
-    
-    def plot_data(self, data_set, time):
-        mean = np.mean(data_set)
-        means = np.ones(len(data_set))*mean
-        trace_value = {"x": time,
-                     "y": data_set,
-                     "mode": 'lines',
-                     "name": 'value'}
-
-        trace_mean = {"x": time,
-                         "y": means,
-                         "mode": 'lines',
-                         "name": 'mean'}
-        traces = [trace_value,trace_mean]
-        layout = dict(title = "Values with mean",
-                      xaxis = dict(title = 'Time'),
-                      yaxis = dict(title = 'Value')
-                     )
-        fig = dict(data=traces, layout=layout)
-        iplot(fig)
-        
-        normalized_data = data_set - mean
-        trace_value = {"x": time,
-                     "y": normalized_data,
-                     "mode": 'lines',
-                     "name": 'value'}
-        traces = [trace_value]
-        layout = dict(title = "After removing mean",
-                      xaxis = dict(title = 'Time'),
-                      yaxis = dict(title = 'Value')
-                     )
-        fig = dict(data=traces, layout=layout)
-        # iplot(fig)
-    
-    def print_stats(self,data,prediction):
-        print("Mean Square Error : " + str(self.mse(data,prediction)))
-        print("Mean of real values : " + str(np.mean(data)))
-        print("Standard Deviation of real values : " + str(np.std(data, ddof=1)))
-        print("Mean of predicted values : " + str(np.mean(prediction)))
-        print("Standard Deviation of predicted values : " + str(np.std(prediction, ddof=1)))
-        print("Number of data points : " + str(len(data)))
-    
-    def plot_result(self, time, data, prediction):
-        trace_real = {"x": time,
-                     "y": data,
-                     "mode": 'lines',
-                     "name": 'Real value'}
-
-        trace_predicted = {"x": time,
-                         "y": prediction,
-                         "mode": 'lines',
-                         "name": 'Predicted value'}
-        traces = [trace_real,trace_predicted]
-        layout = dict(title = "Training Data Set with AR("+str(self.p)+")",
-                      xaxis = dict(title = 'Time'),
-                      yaxis = dict(title = 'Value')
-                     )
-        fig = dict(data=traces, layout=layout)
-        # iplot(fig)
-        self.print_stats(data,prediction)
-        self.plot_residuals(data,prediction)
-    
     def get_anomaly_scores(self, data, prediction):
         mse = np.zeros(len(prediction))
         for i in range(0, len(prediction)):
@@ -232,5 +91,6 @@ class AR:
                 mse_value = -1*mse_value
             mse[i] = mse_value
         max_mse = np.max(mse)
+        if max_mse == 0:
+            return mse
         return mse/max_mse
-    
--- nab/detectors/TSA_AR/TSA_AR_detector.py
+++ nab/detectors/TSA_AR/TSA_AR_detector.py
@@ -9,32 +9,45 @@ class TSA_ARDetector(AnomalyDetector):
     super(TSA_ARDetector, self).__init__(*args, **kwargs)
     self.count = 0
     self.anomaly_scores = None
-
+    self.ar_model = None
+    self.input_data = []
+    self.predictions = []
 
   def handleRecord(self, inputData):
     """Returns a tuple (anomalyScore).
     The anomalyScore is simply a random value from 0 to 1
     """
-    anomalyScore = self.anomaly_scores[self.count]
+
+    self.input_data.append(inputData['value'])
+
+    self.ar_model.set_training_data_set(self.input_data)
+    self.ar_model.calculate_weights()
+    
+    prediction = self.ar_model.get_single_prediction(self.input_data,self.count)
+    self.predictions.append(prediction)
+    
+    # if self.count == 0 :
+    #   print(self.input_data)
+    #   print(self.predictions)
+    
+    anomalyScore = self.ar_model.get_anomaly_scores(self.input_data, self.predictions)[-1]
+    
+    # # anomalyScore = self.anomaly_scores[self.count]
     self.count += 1
-    # print("handle Record : "+str(self.count) )
-    # print(inputData)
+    # anomalyScore = 0.0
+    print(self.count)
     return (anomalyScore, )
 
-
   def initialize(self):
-    # values = self.dataSet.data[list(self.dataSet.data)[1]]
     values = self.dataSet.data["value"]
-    training_set_values = np.array(values[0:int(self.probationaryPeriod)])
+    
     print("probationaryPeriod : "+str(int(self.probationaryPeriod)))
-    # validation_set_values = np.array(values[int(self.probationaryPeriod):-1])
+    training_set_values = np.array(values[0:int(self.probationaryPeriod)])
     validation_set_values = np.array(values)
     
     ar_model = AR(1)
     ar_model.set_training_data_set(training_set_values)
-    print("training_set shape : "+str(training_set_values.shape))
     ar_model.set_validation_data_set(validation_set_values)
-    print("validation_set shape : "+str(validation_set_values.shape))
 
     epochs = 30
     mse = np.zeros(epochs-1)
@@ -44,16 +57,19 @@ class TSA_ARDetector(AnomalyDetector):
         prediction = ar_model.get_prediction(ar_model.training_data)
         mse[i-1] = ar_model.get_mse(ar_model.training_data, prediction)
     
-    best_p = np.argmin(mse)+1
-    print(mse)
+    best_p = int(np.argmin(mse))+1
+    # print(mse)
     print("MSE is minimum at P = "+str(best_p))
     ar_model.set_p(best_p)
-    ar_model.calculate_weights()
-    print("Making AR prediction ...")
-    prediction = ar_model.get_prediction(ar_model.validation_data_set)
-    print("Calculating AR Anomaly Score ...")
-    anomaly_scores = ar_model.get_anomaly_scores(ar_model.validation_data_set, prediction)
+    # ar_model.calculate_weights()
+    # print("Making AR prediction ...")
+    # prediction = ar_model.get_prediction(ar_model.validation_data_set)
+    # print("Calculating AR Anomaly Score ...")
+    # anomaly_scores = ar_model.get_anomaly_scores(ar_model.validation_data_set, prediction)
 
-    self.anomaly_scores = anomaly_scores
+    self.ar_model = ar_model
+    # self.anomaly_scores = anomaly_scores
     self.count = 0
+    self.input_data = []
+    self.predictions = []
     print("Done")
